a3cc9b67ba6a2668483f5239aa8ad04315882bf8
qdl <qdl.cs@qq.com>
2023-09-23 23:14:03 +0800

    add reserved max gpu limitation

--- pjkill/pjkill.py
+++ pjkill/pjkill.py
@@ -7,6 +7,7 @@ from collections import Counter
 from datetime import datetime
 from rich.table import Table
 from typing import Dict
+import pandas as pd
 import subprocess
 import argparse
 import sys
@@ -21,7 +22,7 @@ VERSION = "0.0.1"
 
 # * table and states
 STATES = ["🎄", "🧨"]
-STYLES = ["cyan", "magenta", "green", "yellow", "red", "blue", "cyan", "magenta", "green", "yellow"]
+STYLES = ["cyan", "magenta", "green", "yellow", "red", "blue", "cyan", "magenta", "green", "yellow", "cyan"]
 
 # * JOBID VIRTUAL_PARTITION NAME QUOTA_TYPE USER PHX_PRIORITY ST TIME NODES TOTAL_GRES NODELIST(REASON)
 HEADER = ["jobid", "partition", "name", "quota_type", "user", "priority", "time", "total_gres", "node"]
@@ -36,7 +37,6 @@ BST_CMD = "echo {} | sudo -S scontrol write batch_script {}"  # * scontrol write
 # * kill rules
 TARGETS = ["jupyter"]  # * kill the job if targets in name & runtime > timeout
 
-
 def init_args() -> Dict:
     """Parse and return the arguments."""
     parser = argparse.ArgumentParser(description="sweep all jobs on a partition and kill the timeout process.")
@@ -45,7 +45,8 @@ def init_args() -> Dict:
     parser.add_argument("--type", type=str, default="reserved", help="reserved | spot, reserved by default")
     parser.add_argument("--cycle", type=int, default=60, help="pjkill run every cycle time in minute, 60 by default")
     parser.add_argument("--timeout", type=int, default=10, help="timeout in hour, 10 by default")
-    parser.add_argument("--ngpu", type=int, default=2, help="gpu limit of every job, 2 by default")
+    parser.add_argument("--jp_ngpu", type=int, default=2, help="gpu limit of every job, 2 by default")
+    parser.add_argument("--total_ngpu", type=int, default=8, help="gpu limit of every user, 8 by default")
     parser.add_argument("--njob", type=int, default=2, help="job number limit of every user, 2 by default")
     parser.add_argument("--sweep", action="store_true", help="sweep around every cycle, False by default")
     parser.add_argument("--unkill", action="store_true", help="unkill the job to stay safe False by default")
@@ -53,30 +54,9 @@ def init_args() -> Dict:
     args = vars(parser.parse_args())
     return args
 
-
-def get_jobs(user="$", partition="optimal", type="reserved", logger=None) -> dict:
-    """get all the job infomation of nodes"""
-    try:
-        lines = subprocess.check_output(SEQ_CMD.format(partition, type, user), shell=True).decode("ascii")
-        logger.info(lines)
-        lines = lines.split("\n")
-    except:
-        return {}
-    jobs = {k: [] for k in HEADER}
-
-    for line in lines[:-1]:
-        # * filter out the space and empty string
-        values = list(filter(lambda x: x != "", line.split(" ")))
-        values = [values[i] for i in valids]
-        for k, v in zip(HEADER, values):
-            jobs[k].append(v)
-    return jobs
-
-
 def sec_runtime(time_str):
     pattern = r"^(?:(\d+)-)?(?:([01]?\d|2[0-3]):)?([0-5]?\d):([0-5]?\d)$"
     match = re.match(pattern, time_str)
-
     if match:
         days = int(match.group(1) or 0)
         hours = int(match.group(2) or 0)
@@ -87,6 +67,26 @@ def sec_runtime(time_str):
     else:
         return "Invalid time format"
 
+def get_jobs(user="$", partition="optimal", type="reserved", logger=None) -> dict:
+    """get all the job infomation of nodes"""
+    try:
+        lines = subprocess.check_output(SEQ_CMD.format(partition, type, user), shell=True).decode("ascii")
+        logger.info(lines)
+        lines = lines.split("\n")
+    except:
+        return {}
+    jobs = {k: [] for k in HEADER + ["stime"]}
+
+    for line in lines[:-1]:
+        # * filter out the space and empty string
+        values = list(filter(lambda x: x != "", line.split(" ")))
+        values = [values[i] for i in valids]
+        for k, v in zip(HEADER, values):
+            if k == "time":
+                jobs["stime"].append(sec_runtime(v))
+            jobs[k].append(v)
+    jobs = pd.DataFrame(jobs)
+    return jobs
 
 def is_sbatch(cmd):
     return "/mnt/" in cmd and (" " not in cmd)
@@ -116,21 +116,18 @@ def is_target_job(jobid):
     return in_target, cmd
 
 
-def kill_jobs(timeout, jobs: Dict, args, logger=None):
+def kill_jp_jobs(timeout, jobs: pd.DataFrame, args, logger=None):
     """kill the timeout process"""
     # * sort all job key:value by time
-    times = list(map(sec_runtime, jobs["time"]))
-
-    for k, v in jobs.items():
-        jobs[k] = list(v for _, v in sorted(zip(times, v), key=lambda x: x[0], reverse=True))
+    jobs = jobs.sort_values(by="stime", ascending=False)
 
     # * query job target and count user number in target
     job_valids, cmds = zip(*map(is_target_job, jobs["jobid"]))
-    user_num = Counter([user for user, valid in zip(jobs["user"], job_valids) if valid])
+    user_num = Counter([user for user, valid in zip(jobs["user"], job_valids) if valid]) # * {'user1': 2, 'user2': 1}
 
-    jobs["status"] = []
-    for i, ts in enumerate(jobs["time"]):
-        runtime = sec_runtime(ts)
+    # * init status
+    jobs["status"] = [STATES[0]] * len(jobs["jobid"])
+    for i, runtime in enumerate(jobs.stime):
         user, in_target, cmd, jobid = jobs["user"][i], job_valids[i], cmds[i], jobs["jobid"][i]
 
         """ * kill the job if in target and:
@@ -143,24 +140,55 @@ def kill_jobs(timeout, jobs: Dict, args, logger=None):
             try:
                 if not args["unkill"]:
                     subprocess.check_output(KILL_CMD.format(os.environ["SUDO_PASSWD"], jobid), shell=True)
-                jobs["status"].append(STATES[1])
+                jobs["status"] = STATES[1]
                 logger.info(f"== jobid: [{jobid}], cmd: [{cmd}], was killed")
 
                 if user_num[jobs["user"][i]] > args["njob"]:
                     user_num[jobs["user"][i]] -= 1
             except:
-                jobs["status"].append(STATES[0])
                 logger.info(f"== jobid: {jobid}, cmd: {cmd}, killing failed!")
         else:
-            jobs["status"].append(STATES[0])
             logger.info(f"== jobid: [{jobid}], cmd: [{cmd}], stays safe")
 
         # * add ratio
         jobs["time"][i] += " / {}".format("{:d}-{:02d}:{:02d}:{:02d}".format(timeout // 24, timeout % 24, 0, 0))
     return jobs
 
+def kill_reserved_jobs(jobs: pd.DataFrame, args, logger=None):
+    """kill the num exceed jobs"""
+    # * sort all job key:value by time
+    jobs = jobs.sort_values(by="stime", ascending=True)
 
-def viz_jobs(jobs: Dict):
+    # * query job target and count user number in target
+    user_ngpu = {k: 0 for k in jobs['user'].unique().tolist()}
+    job_valids = [False] * len(jobs["jobid"])
+
+    for i, user in enumerate(jobs['user']):
+        print(f"== status: {jobs['status'][i]}, quota_type: {jobs['quota_type'][i]}, priority: {jobs['priority'][i]}")
+        if jobs["status"][i] == STATES[0] and jobs["quota_type"][i] == "reserved" and jobs["priority"][i] == "normal":
+            user_ngpu[user] += int(jobs["total_gres"][i].split(":")[-1])
+            job_valids[i] = True
+        
+    for i, user in enumerate(jobs["user"]):
+        """ *kill the job if exceed the number limit. """
+        jobid = jobs["jobid"][i]
+
+        if user_ngpu[user] > args["total_ngpu"] and job_valids[i]:
+            try:
+                if not args["unkill"]:
+                    subprocess.check_output(KILL_CMD.format(os.environ["SUDO_PASSWD"], jobid), shell=True)
+                
+                jobs["status"][i] = STATES[1]
+                logger.info(f"== jobid: [{jobid}], exceeded maxgpu number, was killed")
+
+                user_ngpu -= int(jobs["total_gres"][i].split(":")[-1])
+            except:
+                logger.info(f"== jobid: {jobid}, exceeded maxgpu number, killing failed!")
+        else:
+            logger.info(f"== jobid: [{jobid}], under maxgpu number, stays safe")
+    return jobs
+
+def viz_jobs(jobs):
     """visualize the jobs"""
     console = Console()
     table = Table(title="PJKILLER is Sweeping the Job List!", show_header=True, header_style="bold magenta")
@@ -174,9 +202,15 @@ def viz_jobs(jobs: Dict):
 
 def threads_job(args, logger):
     jobs = get_jobs(args["user"], args["partition"], args["type"], logger)
-    njobs = kill_jobs(args["timeout"], jobs, args, logger)
+
+    # * kill jupyter jobs
+    njobs = kill_jp_jobs(args["timeout"], jobs, args, logger)
+
+    # * kill reserved jobs
+    njobs = kill_reserved_jobs(njobs, args, logger)
+
     logger.info("\n")
-    viz_jobs(njobs)
+    viz_jobs(njobs.drop('stime', axis=1))
 
 
 def main():
